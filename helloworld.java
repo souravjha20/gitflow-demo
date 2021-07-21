a95579ebd2b0bfa2ca11bb1da1cc8671bb93aef7
Sourav Jha <Sourav.jha@gmail.com>
2021-07-21 02:27:16 +0000

    file changed

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,8 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");
      Systm.out.println("Welcome to Git");  
+     System.out.println("Hello Java, Welcome to git");  
+
     }  
 }  
 
