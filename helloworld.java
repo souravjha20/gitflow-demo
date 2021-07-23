b8359369a658145ac0b0da0d99fd75c522c99953
Sourav Jha <Sourav.jha@gmail.com>
2021-07-23 02:00:40 +0000

    commit made

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,5 @@
 class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");
-     Systm.out.println("Welcome to Git");  
+    public static void main(String args[]){ 
      System.out.println("Hello Java, Welcome to git");  
 
     }  
