06cdcc90b68cb2c1db40da0c4d0cb04cd21f521e
Sourav Jha <sourav.jha@gmail.com>
2021-07-24 02:20:12 +0000

    removed a line in helloworld.java file

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,6 @@
 class Simple{  
     public static void main(String args[]){ 
-     System.out.println("Hello Java, Welcome to git");
-     System.out.println("Welocme to Git flow"); 
+     System.out.println("Hello Java, Welcome to git"); 
     }  
 }  
 
